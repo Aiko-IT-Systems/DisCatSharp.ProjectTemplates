014eb751d3b3780590883f3e85ae5407002539d9
JBraunsmaJr <jonbraunsma@gmail.com>
2021-11-20 00:27:30 -0500

    Shifted project structure to use git-tagging to automate version numbering Updated classifications

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,5 +1,10 @@
 $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
 $srcDir = (Join-Path -path $scriptDir src)
+$original = (Join-Path -path $srcDir "DisCatSharpTemplates.nuspec")
+$versioned = (Join-Path -path $srcDir "DisCatSharpTemplates-versioned.nuspec")
+
+$tag=(git describe --tags --abbrev=0)
+
 # nuget.exe needs to be on the path or aliased
 function Reset-Templates{
     [cmdletbinding()]
@@ -24,24 +29,43 @@ function Clean(){
     }
 }
 
+function SetupNuspec(){
+    if (Test-Path -LiteralPath $versioned)
+	{
+		Write-Host "Removing Old $versioned..."
+		Remove-Item -Recurse -Force $versioned
+	}
+
+    Copy-Item -Path $original -Destination $versioned
+    (Get-Content $versioned).replace('%VERSION%', $tag) | Set-Content $versioned
+}
+
+function ResetSpec(){
+    if (Test-Path -LiteralPath $versioned)
+	{
+		Write-Host "Removing Old $versioned..."
+		Remove-Item -Recurse -Force $versioned
+	}
+}
+
 # we need to ensure our solution template is generated / updated prior to final package
 Start-Process powershell "$scriptDir\update-solution-template.ps1"
 
 # start script
 Clean
+SetupNuspec
 
 # create nuget package
 $outputpath = Join-Path $scriptDir nupkg
-$pathtonuspec = Join-Path $srcDir DisCatsharpTemplates.nuspec
-if(Test-Path $pathtonuspec){
-    nuget pack $pathtonuspec -OutputDirectory $outputpath
+if(Test-Path $versioned){
+    nuget pack $versioned -OutputDirectory $outputpath
 }
 else{
-    'ERROR: nuspec file not found at {0}' -f $pathtonuspec | Write-Error
+    'ERROR: nuspec file not found at {0}' -f $versioned | Write-Error
     return
 }
 
-$pathtonupkg = join-path $scriptDir nupkg/DisCatSharp.ProjectTemplates.1.0.2.nupkg
+$pathtonupkg = join-path $scriptDir "nupkg/DisCatSharp.ProjectTemplates.$tag.nupkg"
 # install nuget package using dotnet new --install
 if(test-path $pathtonupkg){   
     Reset-Templates
@@ -51,3 +75,5 @@ if(test-path $pathtonupkg){
 else{
     'Not installing template because it was not found at "{0}"' -f $pathtonupkg | Write-Error
 }
+
+ResetSpec
